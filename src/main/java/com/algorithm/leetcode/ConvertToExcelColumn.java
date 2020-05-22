e11e793255645d89cc3067f66ad9acaf8016a551
zhanglifeng <514760469@qq.com>
2020-05-22 14:16:36 +0800

    322. 零钱兑换

--- src/main/java/com/algorithm/leetcode/ConvertToExcelColumn.java
+++ src/main/java/com/algorithm/leetcode/ConvertToExcelColumn.java
@@ -8,26 +8,14 @@ package com.algorithm.leetcode;
 public class ConvertToExcelColumn {
 
     public static void main(String[] args) {
-//        char cc = (char) ('A' + 1 - 1);
-//        System.out.println(cc);
         String convert = new ConvertToExcelColumn().convertToTitle(28);
         System.out.println(convert);
     }
 
     /**
      * n
-     * 26 / 26 = 1
-     * 26 % 26 = 0
-     *
-     * 28 / 26 = 1;
-     * 28 % 26 = 2;
-     *
-     * 701 / 26 = 26
-     * 701 % 26 = 25
      */
     public String convertToTitle(int n) {
-//        int c = n / 26;
-//        int r = n % 26;
         StringBuilder sb = new StringBuilder();
 
         while (n != 0) {
